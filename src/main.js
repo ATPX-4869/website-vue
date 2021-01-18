5c61c3dc7f506d32d8eef79e1d5769df99cf3a59
TeacherZhang <2442652360@qq.com>
2021-01-18 21:50:11 +0800

    first

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+// 导入 Element_ui
+import ElementUI from 'element-ui'
+// 插件 Element-ui
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
